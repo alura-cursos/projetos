4ab61da022493e923803e155cba41997c46a74b3
Guilherme Lima <guilherme.lima@alura.com>
2021-03-16 11:38:54 -0300

    dia 6

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
-function adicionarFilme() {
-    var campoFilmeFavorito = document.querySelector('#filme')
-    var filmeFavorito = campoFilmeFavorito.value
-    if (filmeFavorito.endsWith('.jpg')) {
-        listarFilmesNaTela(filmeFavorito)
-    } else {
-        alert("Imagem inválida")
-    }
-    campoFilmeFavorito.value = ""
+var paulo = {
+    nome: "Paulo",
+    vitorias: 2,
+    empates: 5,
+    derrotas: 1,
+    pontos: 0
+}
+var rafa = {
+    nome: "Rafa",
+    vitorias: 3,
+    empates: 5,
+    derrotas: 2,
+    pontos: 0
 }
 
-function listarFilmesNaTela(filme) {
-    var listaFilmes = document.querySelector('#listaFilmes')
-    var elementoFilme = "<img src=" + filme + ">"
-    listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+rafa.pontos = calculaPontos(rafa)
+paulo.pontos = calculaPontos(paulo)
+
+function calculaPontos(jogador) {
+    var pontos = (jogador.vitorias * 3) + jogador.empates
+    return pontos
+}
+
+var jogadores = [rafa, paulo]
+
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores) {
+    var html = ""
+    for (var i = 0; i < jogadores.length; i++) {
+        html += "<tr><td>" + jogadores[i].nome + "</td>"
+        html += "<td>" + jogadores[i].vitorias + "</td>"
+        html += "<td>" + jogadores[i].empates + "</td>"
+        html += "<td>" + jogadores[i].derrotas + "</td>"
+        html += "<td>" + jogadores[i].pontos + "</td>"
+        html += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+        html += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+        html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></tr>"
+    }
+    var tabelaJogadores = document.getElementById('tabelaJogadores')
+    tabelaJogadores.innerHTML = html
+}
+
+function adicionarVitoria(i) {
+    var jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i) {
+    var jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i) {
+    var jogador = jogadores[i]
+    jogador.derrotas++
+    exibirJogadoresNaTela(jogadores)
 }
