1b2a726a4654e4528c9ef4fd9769a7f5d3071e8e
Guilherme Lima <guilherme.lima@alura.com>
2021-03-16 11:22:21 -0300

    dia 2

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-var valorEmDolarTexto = prompt("Qual o valor em dolar que você quer converter?")
+var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
+var segundoValor = parseInt(prompt("Digite o segundo valor:"))
 
-var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+var operacao = prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração: ")
 
-var valorEmReal = valorEmDolarNumero * 5.50
-var valorEmRealFixado = valorEmReal.toFixed(2)
+if (operacao == 1) {
+  var resultado = primeiroValor / segundoValor
+  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 2) {
+  var resultado = primeiroValor * segundoValor
+  document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 3) {
+  var resultado = primeiroValor + segundoValor
+  document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 4) {
+  var resultado = primeiroValor - segundoValor
+  document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+} else {
+  document.write("<h2>Opção inválida</h2>")
+}
 
-alert(valorEmRealFixado)
+//escrevendo na tela - document.write()
+//concatenação (juntar palavra com variáveis) - ("palavra" + variavel)
+// == - comparação é diferente do = que usamos para fazer atribuição
 
-//isso é um comentário 
+//if = se
+//else = senão
+//else if = senão se
