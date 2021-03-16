c244e0e7f79879ea6aaa250e48dc3ed31c4fc35c
Guilherme Lima <guilherme.lima@alura.com>
2021-03-16 11:29:01 -0300

    dia 5

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+function adicionarFilme() {
+    var campoFilmeFavorito = document.querySelector('#filme')
+    var filmeFavorito = campoFilmeFavorito.value
+    if (filmeFavorito.endsWith('.jpg')) {
+        listarFilmesNaTela(filmeFavorito)
+    } else {
+        alert("Imagem inválida")
+    }
+    campoFilmeFavorito.value = ""
+}
 
-for (var i = 0; i < listaFilmes.length; i++){
-  document.write("<img src=" + listaFilmes[i] + ">")
+function listarFilmesNaTela(filme) {
+    var listaFilmes = document.querySelector('#listaFilmes')
+    var elementoFilme = "<img src=" + filme + ">"
+    listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
 }
