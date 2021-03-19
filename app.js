370147b8e1c8af143b1eb2e7e47e2b391ef0e016
Guilherme Lima <guilherme.lima@alura.com>
2021-03-19 12:14:12 -0300

    dia 9

--- app.js
+++ app.js
@@ -28,29 +28,95 @@ var cartaGui = {
     }
 }
 
+var cartaLol = {
+    nome: "Caitlyn",
+    imagem: "http://1.bp.blogspot.com/-K7CbqWc1-p0/VLc98v85s0I/AAAAAAAABqk/-ZB684VVHbg/s1600/Caitlyn_OriginalSkin.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 40,
+        magia: 10
+    }
+}
+
+var cartaNaruto = {
+    nome: "Naruto",
+    imagem: "https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png",
+    atributos: {
+        ataque: 80,
+        defesa: 60,
+        magia: 100
+    }
+}
+
+var cartaHarry = {
+    nome: "Harry Potter",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/89ff10dd-aa41-4d17-ae8f-835281ebd3fd_49hp.jpg",
+    atributos: {
+        ataque: 70,
+        defesa: 50,
+        magia: 95
+    }
+}
+
+var cartaBatman = {
+    nome: "Batman",
+    imagem: "https://assets.b9.com.br/wp-content/uploads/2020/09/Batman-issue86-heder-1280x677.jpg",
+    atributos: {
+        ataque: 95,
+        defesa: 70,
+        magia: 0
+    }
+}
+
+var cartaMarvel = {
+    nome: "Capitã Marvel",
+    imagem: "https://cinepop.com.br/wp-content/uploads/2018/09/capitamarvel21.jpg",
+    atributos: {
+        ataque: 90,
+        defesa: 80,
+        magia: 0
+    }
+}
 
 var cartaMaquina
 var cartaJogador
-var cartas = [cartaPaulo, cartaRafa, cartaGui]
-// 0          1           2
+var cartas = [cartaPaulo, cartaRafa, cartaGui, cartaLol, cartaNaruto, cartaHarry, cartaBatman, cartaMarvel]
+//            0           1           2          3         4            5            6           7          
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas() {
+    var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+    var html = "Quantidade de cartas no jogo: " + cartas.length
+
+    divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+    var divPlacar = document.getElementById('placar')
+    var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Máquina"
+
+    divPlacar.innerHTML = html
+}
 
 function sortearCarta() {
-    var numeroCartaMaquina = parseInt(Math.random() * 3)
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
     cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
 
-    var numeroCartaJogador = parseInt(Math.random() * 3)
-    while (numeroCartaJogador == numeroCartaMaquina) {
-        numeroCartaJogador = parseInt(Math.random() * 3)
-    }
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
     cartaJogador = cartas[numeroCartaJogador]
-    console.log(cartaJogador)
+    cartas.splice(numeroCartaJogador, 1)
 
     document.getElementById('btnSortear').disabled = true
     document.getElementById('btnJogar').disabled = false
 
     exibeCartaJogador()
 }
-
 
 function exibeCartaJogador() {
     var divCartaJogador = document.getElementById("carta-jogador")
@@ -83,14 +149,33 @@ function jogar() {
 
     if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
         htmlResultado = '<p class="resultado-final">Venceu</p>'
+        pontosJogador++
     } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
         htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        pontosMaquina++
     } else {
         htmlResultado = '<p class="resultado-final">Empatou</p>'
     }
 
+    if (cartas.length == 0) {
+        alert("Fim de jogo")
+        if (pontosJogador > pontosMaquina) {
+            htmlResultado = '<p class="resultado-final">Venceu</p>'
+        } else if (pontosMaquina > pontosJogador) {
+            htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        } else {
+            htmlResultado = '<p class="resultado-final">Empatou</p>'
+        }
+    } else {
+        document.getElementById('btnProximaRodada').disabled = false
+    }
+
     divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+
+    atualizaPlacar()
     exibeCartaMaquina()
+    atualizaQuantidadeDeCartas()
 }
 
 function exibeCartaMaquina() {
@@ -108,4 +193,17 @@ function exibeCartaMaquina() {
     var html = "<div id='opcoes' class='carta-status --spacing'>"
 
     divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada() {
+    var divCartas = document.getElementById('cartas')
+
+    divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnJogar').disabled = true
+    document.getElementById('btnProximaRodada').disabled = true
+
+    var divResultado = document.getElementById('resultado')
+    divResultado.innerHTML = ""
 }
